a7a7a3f1588112e7154efd071e38ce3aa09f57ad
DavidSebas20 <davidsebastian20@outlook.com>
2025-01-22 18:53:45 -0500

    :tada: initialize readpatients application with Maven setup and basic configuration

---
+++ src/main/java/com/example/readpatients/ReadpatientsApplication.java
@@ -0,0 +1,13 @@
+package com.example.readpatients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadpatientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadpatientsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/readpatients/ReadpatientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.readpatients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadpatientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
