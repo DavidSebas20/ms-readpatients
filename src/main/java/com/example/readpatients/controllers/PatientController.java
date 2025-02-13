8520aa112c4aa26096b2b22312dac6ea1bf19aab
DavidSebas20 <davidsebastian20@outlook.com>
2025-02-13 14:01:14 -0500

    ✨ :heavy_plus_sign: update PatientController request mapping to correct 'read-patient' prefix

--- src/main/java/com/example/readpatients/controllers/PatientController.java
+++ src/main/java/com/example/readpatients/controllers/PatientController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/read-patients/patients")
+@RequestMapping("/read-patient/patients")
 public class PatientController {
 
     @Autowired
